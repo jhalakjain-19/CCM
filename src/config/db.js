e0cedf8b56bc4cea02a9889981c5fb760de5f665
jhalak jain <jhalak.jain@irbureau.com>
2025-01-08 10:43:49 +0530

    mysql2/promise

--- src/config/db.js
+++ src/config/db.js
@@ -1,23 +1,21 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const dotenv = require("dotenv").config();
-
-const pool = mysql
-  .createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-  })
-  .promise();
+//Main DB configuration
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
 // // Test Connection
-// pool.getConnection((err, connection) => {
-//   if (err) {
-//     console.error("Error connecting to main database:", err.message);
-//   } else {
-//     console.log("Connected to main database");
-//     connection.release(); // Release the connection back to the pool
-//   }
-// });
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to main database:", err.message);
+  } else {
+    console.log("Connected to main database");
+    connection.release(); // Release the connection back to the pool
+  }
+});
 
 module.exports = pool;
