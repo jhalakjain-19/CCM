27734572560742c29b33cc563f741c88989d9a6f
jhalak jain <jhalak.jain@irbureau.com>
2025-03-03 13:16:35 +0530

    multer

---
+++ src/middlewares/uploadMiddleware.js
@@ -0,0 +1,19 @@
+const multer = require("multer");
+
+const storage = multer.memoryStorage(); // Store file in memory
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype ===
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+      file.mimetype === "application/vnd.ms-excel"
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only Excel files are allowed"), false);
+    }
+  },
+});
+
+module.exports = upload;
